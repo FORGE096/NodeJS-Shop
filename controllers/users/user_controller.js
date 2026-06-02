4f06c7044388d16960059cfb82913b9847aced9c
FORGE096 <forge1803@gmail.com>
2026-06-02 16:47:17 +0330

    some changes in user controller

--- controllers/users/user_controller.js
+++ controllers/users/user_controller.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
 async function buyProduct(request, response) {
-
+    response.send({
+        CODE: response.statusCode,
+        MESSAGE: "Service Working",
+    });
 }
 
 async function updateUser(request, response) {
-
+    response.send({
+        CODE: response.statusCode,
+        MESSAGE: "Service Working",
+    });
 }
 
 module.exports = {
