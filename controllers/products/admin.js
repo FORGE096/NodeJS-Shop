3716dfadb51f5c3f6f7ebf10d8eeb09df178ec98
FORGE096 <forge1803@gmail.com>
2026-06-13 00:21:21 +0330

    feat: admin controller and router updated.
    - update metoud added.
    - delete methoud added.
    - add methoud added.

--- controllers/products/admin.js
+++ controllers/products/admin.js
@@ -1,1 +1,28 @@
 const Product = require("../../models/product");
+
+async function addProduct(request, response) {
+    response.send({
+        CODE: response.statusCode,
+        MESSAGE: "Add Product",
+    });
+}
+
+async function deleteProduct(request, response) {
+    response.send({
+        CODE: response.statusCode,
+        MESSAGE: "Delete Product",
+    });
+}
+
+async function updateProduct(request, response) {
+    response.send({
+        CODE: response.statusCode,
+        MESSAGE: "Update Product",
+    });
+}
+
+module.exports = {
+    addProduct,
+    deleteProduct,
+    updateProduct,
+}
--- routes/users/admin_router.js
+++ routes/users/admin_router.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const admin = require("../../controllers/products/admin");
 
-router.get("/add-product", (request, response) => { });
+router.get("/add-product", admin.addProduct);
+router.get("/delete-product", admin.deleteProduct);
+router.get("/update-product", admin.updateProduct);
 
 module.exports = router;
